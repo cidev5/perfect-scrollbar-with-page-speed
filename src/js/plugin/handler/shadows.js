e5ee3b0de72fb8b885f333ba46c1eaad5a085ad5
Dawid Pawelec <dawid.pawelec@caissallc.com>
2017-08-04 14:07:02 -0400

    Remove throttling from shadows.js and make shadows subtler and shallower

--- src/js/plugin/handler/shadows.js
+++ src/js/plugin/handler/shadows.js
@@ -1,63 +1,60 @@
 'use strict';
 
-var _ = require('../../lib/helper');
 var cls = require('../../lib/class');
 var instances = require('../instances');
 
-var THROTTLE_SPEED = 1500;
-
 function bindReachStartEndEventshandler(element, i) {
-  i.event.bind(element, 'ps-y-reach-start-more', _.throttle(function () {
+  i.event.bind(element, 'ps-y-reach-start-more', function () {
     cls.add(element, 'ps-y-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-start-more', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-start-more', function () {
     cls.add(element, 'ps-x-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-y-reach-end-more', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-y-reach-end-more', function () {
     cls.add(element, 'ps-y-reach-end-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-end-more', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-end-more', function () {
     cls.add(element, 'ps-x-reach-end-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-y-reach-start', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-y-reach-start', function () {
     cls.remove(element, 'ps-y-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-start', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-start', function () {
     cls.remove(element, 'ps-x-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-y-reach-end', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-y-reach-end', function () {
     cls.remove(element, 'ps-y-reach-end-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-end', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-end', function () {
     cls.remove(element, 'ps-x-reach-end-more');
-  }), THROTTLE_SPEED);
+  });
 }
 
 function bindReachStartEndEventshandlerMulti(element, i, elements) {
-  i.event.bind(element, 'ps-y-reach-start-more', _.throttle(function () {
+  i.event.bind(element, 'ps-y-reach-start-more', function () {
     cls.addMulti(elements, 'ps-y-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-start-more', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-start-more', function () {
     cls.addMulti(elements, 'ps-x-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-y-reach-end-more', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-y-reach-end-more', function () {
     cls.addMulti(elements, 'ps-y-reach-end-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-end-more', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-end-more', function () {
     cls.addMulti(elements, 'ps-x-reach-end-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-y-reach-start', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-y-reach-start', function () {
     cls.removeMulti(elements, 'ps-y-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-start', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-start', function () {
     cls.removeMulti(elements, 'ps-x-reach-start-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-y-reach-end', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-y-reach-end', function () {
     cls.removeMulti(elements, 'ps-y-reach-end-more');
-  }), THROTTLE_SPEED);
-  i.event.bind(element, 'ps-x-reach-end', _.throttle(function () {
+  });
+  i.event.bind(element, 'ps-x-reach-end', function () {
     cls.removeMulti(elements, 'ps-x-reach-end-more');
-  }), THROTTLE_SPEED);
+  });
 }
 
 module.exports = function (element, extraElements) {
